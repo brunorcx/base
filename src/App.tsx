1155623a9257c2aed38769ee0972c39cf8ea0206
Bruno RC <brunorcx@hotmail.com>
2021-01-19 18:32:19 -0400

    Primeiro commit app-rest

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -18,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+          <h1>Teste react</h1>
+        </div>
       </header>
     </div>
   );
