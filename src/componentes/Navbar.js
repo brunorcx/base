0581b0650ef0de0d519c4a14e3a243af86de4732
Bruno RC <brunorcx@hotmail.com>
2021-03-03 19:46:40 -0400

    Header e Logo

--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -6,7 +6,22 @@ import { Link } from "react-router-dom";
 
 export class Navbar extends Component {
   render() {
-    return <header>Header</header>;
+    return (
+      <header>
+        <div className="logo">LOGO</div>
+        <div className="navbar">
+          <AiOutlineMenu className="menu" size="2rem" />
+          <ul>
+            <li>
+              <Link to="/">Produtos</Link>
+            </li>
+            <li>
+              <Link to="/">Usuários</Link>
+            </li>
+          </ul>
+        </div>
+      </header>
+    );
   }
 }
 export default Navbar;
