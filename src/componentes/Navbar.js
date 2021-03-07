a6c6d4db752c5bc15b9ed82b39b85ce7a2c55b02
Machado21 <grmachado12@hotmail.com>
2021-03-07 19:16:54 -0400

    Add Inout search and stile

--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "../styles/navbar.css";
 import { AiOutlineShoppingCart } from "react-icons/ai";
+import { FaReact } from "react-icons/fa";
 import { AiOutlineMenu } from "react-icons/ai";
 import { Link } from "react-router-dom";
 
@@ -8,7 +9,10 @@ export class Navbar extends Component {
   render() {
     return (
       <header>
-        <div className="logo">LOGO</div>
+        <div className="navbar-logo">
+          <FaReact className="logo" size="2rem" />
+          <input type="search" className="search" placeholder="Search"></input>
+        </div>
         <div className="navbar">
           <AiOutlineMenu className="menu" size="2rem" />
           <ul>
