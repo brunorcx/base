33b5e574b336e5068cbe89cc289a60f8ffc5a434
Machado21 <grmachado12@hotmail.com>
2021-03-26 19:19:31 -0400

    Atualizações de Listprodutos e Footer

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -5,7 +5,12 @@ export class Footer extends Component {
   render() {
     return (
       <div className="footer">
-        <div className="limite">teste</div>
+        <div className="limite">
+          <p>Contato: email</p>
+          <p></p>
+          <p>Sobre Nós</p>
+          <p>Sobre Nós</p>
+        </div>
       </div>
     );
   }
