45f45d33d5831c33555f79260b776dff5853c954
Machado21 <grmachado12@hotmail.com>
2021-04-11 14:22:02 -0400

    Tela de Produto unico

--- src/componentes/Product.js
+++ src/componentes/Product.js
@@ -21,13 +21,27 @@ export class Product extends Component {
                 <li>
                   <img src="https://carrefourbr.vtexassets.com/arquivos/ids/7739094-150-auto?width=150&height=auto&aspect=true" />
                 </li>
-                <li></li>
-                <li></li>
               </ul>
             </div>
-            <div className="img-max">1.2</div>
+            <div className="img-max">
+              <img src="http://carrefourbr.vtexassets.com/arquivos/ids/7739079-150-auto?width=150&height=auto&aspect=true" />
+            </div>
           </div>
-          <div className="infor">2</div>
+          <div className="infor">
+            <h2>
+              Smartphone Samsung Galaxy A31 128GB Azul Tela 6.4 Pol. Câmera 48MP
+              Selfie 20MP Dual Chip Android 10.0
+            </h2>
+            <div className="divider" />
+            <div className="infor-2">
+              <div className="preço">
+                <h2>R$1.000,00</h2>
+              </div>
+              <div className="comprar">
+                <p>Comprar</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
