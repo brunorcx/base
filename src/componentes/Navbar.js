585a5631c57e1ccb6a057216e085121a18139ee9
Bruno RC <brunorcx@hotmail.com>
2021-04-16 12:38:14 -0400

    Ajuste nomes de classes, reorganização da navbar e correção padding no menu

--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -26,15 +26,15 @@ export class Navbar extends Component {
     let carrinho_class = this.state.aberto ? "carrinho-menu" : "carrinho-menu-fechado";
     return (
       <header className="nav">
-        <div className="limite">
-          <div className="navbar1">
-            <FaReact className="logo" size="2rem" color="#1e5bc6" />
-            <div id="div-search" className="div-search">
-              <input type="text" className="search" placeholder="Search"></input>
-            </div>
-            <div className="lupa">
-              <BsSearch className="lupa-icon" size="1.5rem" color="#fff" />
-            </div>
+        <div className="navbar1">
+          <FaReact className="logo" size="2rem" color="#1e5bc6" />
+          <div id="div-search" className="div-search">
+            <input type="text" className="search" placeholder="Search"></input>
+          </div>
+          <div className="lupa">
+            <BsSearch className="lupa-icon" size="1.5rem" color="#fff" />
+          </div>
+          <div className="navDireita">
             <div className="div-user">
               <HiOutlineUser className="user-icon" size="2rem" />
               <div className="bem-vindo">
@@ -57,24 +57,22 @@ export class Navbar extends Component {
           </div>
         </div>
         <div className="linha-horizontal" />
-        <div className="limite">
-          <div className="navbar">
-            <AiOutlineMenu className="menu" size="2rem" />
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/Produtos">Produtos</Link>
-              </li>
-              <li>
-                <Link to="/">Usuários</Link>
-              </li>
-              <li>
-                <Link to="/">Contato</Link>
-              </li>
-            </ul>
-          </div>
+        <div className="navbar2">
+          <AiOutlineMenu className="hamburguer" size="2rem" />
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/Produtos">Produtos</Link>
+            </li>
+            <li>
+              <Link to="/">Usuários</Link>
+            </li>
+            <li>
+              <Link to="/">Contato</Link>
+            </li>
+          </ul>
         </div>
       </header>
     );
