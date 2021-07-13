dc14d1aa0873da7eeefec05736fb00f56af5bce0
Bruno RC <brunorcx@hotmail.com>
2021-07-12 20:58:15 -0400

    componente tabela inicial criado para página de cadastro de produtos

--- src/pages/CadastroProdutos.tsx
+++ src/pages/CadastroProdutos.tsx
@@ -1,14 +1,15 @@
 import { Navbar } from "../componentes/Navbar";
 import { Footer } from "../componentes/Footer";
+import DenseTable from "../componentes/tabelaProdutos";
 import "../styles/cadastroProdutos.css";
 import React, { Component } from "react";
-import { MDCDataTable } from "@material/data-table";
 
 export class Produto extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <DenseTable />
         <Footer />
       </div>
     );
