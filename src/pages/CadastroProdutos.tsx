faa4948d1dd49beda6638b42c329981e36f7c061
Bruno RC <brunorcx@hotmail.com>
2021-07-13 20:44:19 -0400

    header e ícone na criação de produtos

--- src/pages/CadastroProdutos.tsx
+++ src/pages/CadastroProdutos.tsx
@@ -3,12 +3,26 @@ import { Footer } from "../componentes/Footer";
 import DenseTable from "../componentes/tabelaProdutos";
 import "../styles/cadastroProdutos.css";
 import React, { Component } from "react";
+import { BsSearch } from "react-icons/bs";
+import { MdAddBox } from "react-icons/md";
 
 export class Produto extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <div className="cUserHeader">
+          <div className="cUserHeaderPart1">Cadastro Produtos</div>
+          <div className="cUserHeaderPart2">
+            <div className="divSearchPequena">
+              <input type="text" className="searchPequena" placeholder="Search"></input>
+              <div className="lupa">
+                <BsSearch className="lupa-icon" size="1.5rem" color="#fff" />
+              </div>
+            </div>
+            <MdAddBox size="1.5rem" color="#fff" />
+          </div>
+        </div>
         <DenseTable />
         <Footer />
       </div>
@@ -18,3 +32,4 @@ export class Produto extends Component {
 
 export default Produto;
 //https://material-ui.com/pt/components/tables/
+//https://codepen.io/7br_uno/pen/XWMjGaw
