4a24473e71759913f9134a01e2101dea45363583
Bruno RC <brunorcx@hotmail.com>
2021-07-25 14:03:57 -0400

    parar rolagem quando abrir cadastro produtos

--- src/pages/CadastroProduto.tsx
+++ src/pages/CadastroProduto.tsx
@@ -13,6 +13,16 @@ export interface CadastroProdutoProps {}
 const CadastroProduto: React.FC<CadastroProdutoProps> = () => {
   const [criarProduto, setCriarProduto] = useState(false); //Valor dentro da função é valor inicial da variável
   console.log(criarProduto);
+  useEffect(() => {
+    if (criarProduto) {
+      // window.onscroll = null;
+      document.documentElement.style.overflow = "hidden";
+      // document.body.scroll = "no"; //Internet Explorer
+    } else {
+      document.documentElement.style.overflow = "auto";
+      // document.body.scroll = "yes"; //Internet Explorer
+    }
+  }, [criarProduto]); // Apenas re-execute o efeito quando o count mudar
 
   // useEffect(() => {
   //   //Executa quando o componente é atualiazado, nesse caso quando criarProduto é modificado
@@ -30,11 +40,7 @@ const CadastroProduto: React.FC<CadastroProdutoProps> = () => {
         <div className="cUserHeaderPart1">Cadastro Produtos</div>
         <div className="cUserHeaderPart2">
           <div className="divSearchPequena">
-            <input
-              type="text"
-              className="searchPequena"
-              placeholder="Search"
-            ></input>
+            <input type="text" className="searchPequena" placeholder="Search"></input>
             <div className="lupa">
               <BsSearch className="lupa-icon" size="1.5rem" color="#fff" />
             </div>
@@ -62,10 +68,7 @@ const CadastroProduto: React.FC<CadastroProdutoProps> = () => {
       <FormProduto novoProduto={criarProduto} />
 
       {criarProduto && (
-        <div
-          className="background_cadastro"
-          onClick={() => setCriarProduto(!criarProduto)}
-        />
+        <div className="background_cadastro" onClick={() => setCriarProduto(!criarProduto)} />
       )}
 
       <DenseTable />
