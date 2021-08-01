a21bc4be31d95cceeef5f0b40bf6ae45f37a9351
brunorcx <brunorcx@hotmail.com>
2021-08-01 13:19:43 -0400

    pegando informações da nova collection

--- src/componentes/tabelaProdutos.js
+++ src/componentes/tabelaProdutos.js
@@ -30,19 +30,19 @@ function createData(name, calories, fat, carbs, protein) {
 }
 
 const rows = [
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Donut", 452, 25.0, 51, 4.9),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Honeycomb", 408, 3.2, 87, 6.5),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Jelly Bean", 375, 0.0, 94, 0.0),
-  createData("KitKat", 518, 26.0, 65, 7.0),
-  createData("Lollipop", 392, 0.2, 98, 0.0),
-  createData("Marshmallow", 318, 0, 81, 2.0),
-  createData("Nougat", 360, 19.0, 9, 37.0),
-  createData("Oreo", 437, 18.0, 63, 4.0),
+  // createData("Cupcake", 305, 3.7, 67, 4.3),
+  // createData("Donut", 452, 25.0, 51, 4.9),
+  // createData("Eclair", 262, 16.0, 24, 6.0),
+  // createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  // createData("Gingerbread", 356, 16.0, 49, 3.9),
+  // createData("Honeycomb", 408, 3.2, 87, 6.5),
+  // createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  // createData("Jelly Bean", 375, 0.0, 94, 0.0),
+  // createData("KitKat", 518, 26.0, 65, 7.0),
+  // createData("Lollipop", 392, 0.2, 98, 0.0),
+  // createData("Marshmallow", 318, 0, 81, 2.0),
+  // createData("Nougat", 360, 19.0, 9, 37.0),
+  // createData("Oreo", 437, 18.0, 63, 4.0),
 ];
 
 function descendingComparator(a, b, orderBy) {
@@ -76,16 +76,24 @@ const headCells = [
     id: "name",
     numeric: false,
     disablePadding: true,
-    label: "Dessert (100g serving)",
+    label: "Fabricante",
   },
-  { id: "calories", numeric: true, disablePadding: false, label: "Calories" },
-  { id: "fat", numeric: true, disablePadding: false, label: "Fat (g)" },
-  { id: "carbs", numeric: true, disablePadding: false, label: "Carbs (g)" },
-  { id: "protein", numeric: true, disablePadding: false, label: "Protein (g)" },
+  { id: "calories", numeric: true, disablePadding: true, label: "Nome" },
+  { id: "fat", numeric: true, disablePadding: false, label: "Preço" },
+  { id: "carbs", numeric: true, disablePadding: false, label: "Quantidade" },
+  { id: "protein", numeric: true, disablePadding: true, label: "Categorias" },
 ];
 
 function EnhancedTableHead(props) {
-  const { classes, onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
+  const {
+    classes,
+    onSelectAllClick,
+    order,
+    orderBy,
+    numSelected,
+    rowCount,
+    onRequestSort,
+  } = props;
   const createSortHandler = (property) => (event) => {
     onRequestSort(event, property);
   };
@@ -168,11 +176,21 @@ const EnhancedTableToolbar = (props) => {
       })}
     >
       {numSelected > 0 ? (
-        <Typography className={classes.title} color="inherit" variant="subtitle1" component="div">
+        <Typography
+          className={classes.title}
+          color="inherit"
+          variant="subtitle1"
+          component="div"
+        >
           {numSelected} selected
         </Typography>
       ) : (
-        <Typography className={classes.title} variant="h6" id="tableTitle" component="div">
+        <Typography
+          className={classes.title}
+          variant="h6"
+          id="tableTitle"
+          component="div"
+        >
           Nutrition
         </Typography>
       )}
@@ -281,7 +299,8 @@ const EnhancedTable = (props) => {
 
   const isSelected = (name) => selected.indexOf(name) !== -1;
 
-  const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
 
   //TABELA TEM CONDIÇÂO PARA NAME SER ÚNICO
   //É possível adicionar elementos repetidos mas isso causa erros na tabelas
@@ -293,11 +312,11 @@ const EnhancedTable = (props) => {
         for (let i = 0; i < 8; i = i + 2) {
           rows.push(
             createData(
-              result[i].items[0].name,
-              result[i].items[0].price.$numberDecimal,
-              result[i].items[0].tags[0],
-              result[i].items[0].quantity,
-              result[i].customer.email
+              result[i].brand,
+              result[i].name,
+              result[i].price,
+              result[i].qty,
+              result[i].tags + " "
             )
           );
         }
@@ -348,7 +367,12 @@ const EnhancedTable = (props) => {
                           inputProps={{ "aria-labelledby": labelId }}
                         />
                       </TableCell>
-                      <TableCell component="th" id={labelId} scope="row" padding="none">
+                      <TableCell
+                        component="th"
+                        id={labelId}
+                        scope="row"
+                        padding="none"
+                      >
                         {row.name}
                       </TableCell>
                       <TableCell align="right">{row.calories}</TableCell>
