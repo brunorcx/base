bf1d106f0e843b67ff5ae4c3012b17cb040d79c4
Bruno RC <brunorcx@hotmail.com>
2021-09-28 20:18:43 -0400

    images exemplo banners no home

--- src/componentes/Propaganda.js
+++ src/componentes/Propaganda.js
@@ -5,13 +5,27 @@ export class Propaganda extends Component {
   render() {
     return (
       <div className="all">
-        <div className="p1">001</div>
-        <div className="p2">002</div>
-        <div className="p3">003</div>
-        <div className="p4">004</div>
-        <div className="p5">005</div>
-        <div className="p6">006</div>
-        <div className="p7">007</div>
+        <div className="p1">
+          <img src="https://www.fastshop.com.br/wcsstore/FastShop/img/home/2021/09/23/intpro-_-230921-_-03-_-FastPrime-_-ND-_-ND-_-ND-_-homedesk.jpg"></img>
+        </div>
+        <div className="p2">
+          <img src="https://carrefourbr.vtexassets.com/assets/vtex.file-manager-graphql/images/0ad00ac0-9bf1-4f4c-b164-63b66a3f258f___6dc7fcef0b6b8d93ffd880ef125088b7.jpg"></img>
+        </div>
+        <div className="p3">
+          <img src="https://carrefourbr.vtexassets.com/assets/vtex.file-manager-graphql/images/1fc6d0e1-fe4a-4db3-a56b-271752118b64___4dbc70c01deb08dfad006c805b6a8b8b.jpg"></img>
+        </div>
+        <div className="p4">
+          <img src="https://carrefourbr.vtexassets.com/assets/vtex.file-manager-graphql/images/6519bac4-a910-4a7d-a543-2a38929aa7fb___1bd4c5da86cf4d104ff5d9a2988528c0.jpg"></img>
+        </div>
+        <div className="p5">
+          <img src="https://carrefourbr.vtexassets.com/assets/vtex.file-manager-graphql/images/8af058b6-1306-4b5b-afbf-099511559d5e___65152e05cb21ce1f64ba2674a29cc36c.jpg"></img>
+        </div>
+        <div className="p6">
+          <img src="https://carrefourbr.vtexassets.com/assets/vtex.file-manager-graphql/images/dc67f16c-5ee5-449b-a30b-38a6c32ccd28___3681655ea8adb442689e462b4e958909.gif"></img>
+        </div>
+        <div className="p7">
+          <img src="https://i5.walmartimages.com/dfw/4ff9c6c9-ba24/k2-_47b5ee96-150f-4c13-922d-6e46b141020a.v1.jpg?odnHeight=222&odnWidth=395&odnBg=FFFFFF"></img>
+        </div>
       </div>
     );
   }
