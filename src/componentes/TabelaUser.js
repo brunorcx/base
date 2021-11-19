d6e7d07b7afc129086769480dcb8748a220babf9
Bruno RC <brunorcx@hotmail.com>
2021-11-18 20:11:44 -0400

    arrumado ícones faltando na tabela

--- src/componentes/TabelaUser.js
+++ src/componentes/TabelaUser.js
@@ -1,8 +1,32 @@
 import MaterialTable from "material-table";
 import { createTheme } from "@material-ui/core/styles";
-<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />;
+import { forwardRef, useState } from "react";
+import { BsSearch } from "react-icons/bs";
+import { MdAddBox, MdOutlineClear } from "react-icons/md";
+import { BiPlus, BiDownArrowAlt, BiChevronLeft, BiChevronRight, BiFirstPage, BiLastPage } from "react-icons/bi";
+const tableIcons = {
+  // Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  // Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  // Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  // Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  // DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  // Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  // Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  // Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <BiFirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <BiLastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <BiChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => <BiChevronLeft {...props} ref={ref} />),
+  ResetSearch: forwardRef((props, ref) => <MdOutlineClear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <BsSearch {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <BiDownArrowAlt size="1.3rem" color="white" {...props} ref={ref} />),
+  // ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  // ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+};
 
 const TabelaUser = () => {
+  const [selectedRow, setSelectedRow] = useState(null);
+
   return (
     <MaterialTable
       title="Basic Selection Preview"
@@ -20,13 +44,26 @@ const TabelaUser = () => {
         { name: "Mehmet", surname: "Baran", birthYear: 1987, birthCity: 63 },
         { name: "Zerya Betül", surname: "Baran", birthYear: 2017, birthCity: 34 },
       ]}
+      onRowClick={(evt, selectedRow) => setSelectedRow(selectedRow.tableData.id)}
       options={{
         selection: true,
+        rowStyle: (rowData) => ({
+          backgroundColor: selectedRow === rowData.tableData.id ? "#1e5cc63f" : "#FFF",
+        }),
         headerStyle: {
           backgroundColor: "#1e5bc6",
           color: "#FFF",
         },
       }}
+      actions={[
+        {
+          icon: BiPlus,
+          tooltip: "Add User",
+          isFreeAction: true,
+          onClick: (event) => alert("You want to add a new row"),
+        },
+      ]}
+      icons={tableIcons}
     />
   );
 };
