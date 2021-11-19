3e9c0fd6f2039309fe80fc04129e83daac081f23
Bruno RC <brunorcx@hotmail.com>
2021-11-18 20:44:56 -0400

    modificado estilo de seleção checkboxes na tabela

--- src/componentes/TabelaUser.js
+++ src/componentes/TabelaUser.js
@@ -4,6 +4,23 @@ import { forwardRef, useState } from "react";
 import { BsSearch } from "react-icons/bs";
 import { MdAddBox, MdOutlineClear } from "react-icons/md";
 import { BiPlus, BiDownArrowAlt, BiChevronLeft, BiChevronRight, BiFirstPage, BiLastPage } from "react-icons/bi";
+import { ThemeProvider } from "@material-ui/styles";
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#4caf50",
+    },
+    secondary: {
+      main: "#ff9100",
+    },
+  },
+  root: {
+    "&:hover": {
+      backgroundColor: "#4caf50",
+    },
+  },
+});
+
 const tableIcons = {
   // Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
   // Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
@@ -28,43 +45,50 @@ const TabelaUser = () => {
   const [selectedRow, setSelectedRow] = useState(null);
 
   return (
-    <MaterialTable
-      title="Basic Selection Preview"
-      columns={[
-        { title: "Name", field: "name" },
-        { title: "Surname", field: "surname" },
-        { title: "Birth Year", field: "birthYear", type: "numeric" },
-        {
-          title: "Birth Place",
-          field: "birthCity",
-          lookup: { 34: "İstanbul", 63: "Şanlıurfa" },
-        },
-      ]}
-      data={[
-        { name: "Mehmet", surname: "Baran", birthYear: 1987, birthCity: 63 },
-        { name: "Zerya Betül", surname: "Baran", birthYear: 2017, birthCity: 34 },
-      ]}
-      onRowClick={(evt, selectedRow) => setSelectedRow(selectedRow.tableData.id)}
-      options={{
-        selection: true,
-        rowStyle: (rowData) => ({
-          backgroundColor: selectedRow === rowData.tableData.id ? "#1e5cc63f" : "#FFF",
-        }),
-        headerStyle: {
-          backgroundColor: "#1e5bc6",
-          color: "#FFF",
-        },
-      }}
-      actions={[
-        {
-          icon: BiPlus,
-          tooltip: "Add User",
-          isFreeAction: true,
-          onClick: (event) => alert("You want to add a new row"),
-        },
-      ]}
-      icons={tableIcons}
-    />
+    <ThemeProvider theme={theme}>
+      <MaterialTable
+        title="Basic Selection Preview"
+        columns={[
+          { title: "Name", field: "name" },
+          { title: "Surname", field: "surname" },
+          { title: "Birth Year", field: "birthYear", type: "numeric" },
+          {
+            title: "Birth Place",
+            field: "birthCity",
+            lookup: { 34: "İstanbul", 63: "Şanlıurfa" },
+          },
+        ]}
+        data={[
+          { name: "Mehmet", surname: "Baran", birthYear: 1987, birthCity: 63 },
+          { name: "Zerya Betül", surname: "Baran", birthYear: 2017, birthCity: 34 },
+        ]}
+        onRowClick={(evt, selectedRow) => setSelectedRow(selectedRow.tableData.id)}
+        options={{
+          selection: true,
+          rowStyle: (rowData) => ({
+            backgroundColor: selectedRow === rowData.tableData.id ? "#1e5cc63f" : "#FFF",
+          }),
+          headerStyle: {
+            backgroundColor: "#1e5bc6",
+            color: "#FFF",
+          },
+        }}
+        actions={[
+          {
+            icon: BiPlus,
+            tooltip: "Add User",
+            isFreeAction: true,
+            onClick: (event) => alert("You want to add a new row"),
+            cellStyle: {
+              hover: {
+                backgroundColor: "green",
+              },
+            },
+          },
+        ]}
+        icons={tableIcons}
+      />
+    </ThemeProvider>
   );
 };
 
