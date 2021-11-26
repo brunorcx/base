059a78eb648b4d0b90d55475ed1541c953b8956d
Bruno RC <brunorcx@hotmail.com>
2021-11-25 22:29:27 -0400

    versão incial sem teste para cadastro de usuário

--- src/componentes/database/FormUsuario.js
+++ src/componentes/database/FormUsuario.js
@@ -15,6 +15,16 @@ const FormUsuario = (props) => {
   const [imagemCarregada, setImagemCarregada] = useState(false); //Vetor de estados
   const [imagemArquivo, setImagemArquivo] = useState(null); //Vetor de estados
 
+  async function Cadastrar(e) {
+    const formData = new FormData();
+    formData.append("name", document.getElementById("name").value);
+    formData.append("password", document.getElementById("password").value);
+    formData.append("email", document.getElementById("email").value);
+    const imageCompressed = await ImageCompression(imagemArquivo);
+    formData.append("image", imageCompressed);
+
+    PostProduto("/users/images", formData);
+  }
   var formUsuarioStyle;
   if (props.novoUsuario) {
     formUsuarioStyle = "formUsuarioAberto";
@@ -22,35 +32,35 @@ const FormUsuario = (props) => {
     formUsuarioStyle = "formUsuarioFechado";
   }
 
-  function addTag() {
-    if (limiter < 6) {
-      limiter++;
+  // function addTag() {
+  //   if (limiter < 6) {
+  //     limiter++;
 
-      setInputs((oldArray) => [
-        ...oldArray,
-        <div className="extra-tag" id={limiter} key={limiter} autoFocus>
-          <BiSubdirectoryRight size="1.4rem" className="enter-tag" />
-          <input key={"cat0" + limiter} type="text" id={"cat0" + limiter} required autoFocus />
-        </div>,
-      ]);
-    } else {
-      alert("O Limite de categorias é 7");
-    }
-    console.log(inputs);
-    console.log(limiter + "Limiter");
-  }
+  //     setInputs((oldArray) => [
+  //       ...oldArray,
+  //       <div className="extra-tag" id={limiter} key={limiter} autoFocus>
+  //         <BiSubdirectoryRight size="1.4rem" className="enter-tag" />
+  //         <input key={"cat0" + limiter} type="text" id={"cat0" + limiter} required autoFocus />
+  //       </div>,
+  //     ]);
+  //   } else {
+  //     alert("O Limite de categorias é 7");
+  //   }
+  //   console.log(inputs);
+  //   console.log(limiter + "Limiter");
+  // }
 
-  function subTag() {
-    if (limiter > 0) {
-      setInputs(inputs.filter((item) => item.props.id !== limiter));
-      limiter--;
-      // setInputs(inputs.filter((item) => item.props.id !== limiter));
-    } else {
-      alert("Deve existir no minimo 1 categoria!");
-    }
-    console.log(inputs);
-    console.log(limiter + "Limiter");
-  }
+  // function subTag() {
+  //   if (limiter > 0) {
+  //     setInputs(inputs.filter((item) => item.props.id !== limiter));
+  //     limiter--;
+  //     // setInputs(inputs.filter((item) => item.props.id !== limiter));
+  //   } else {
+  //     alert("Deve existir no minimo 1 categoria!");
+  //   }
+  //   console.log(inputs);
+  //   console.log(limiter + "Limiter");
+  // }
 
   function onChangeImageHandler(e) {
     console.log(e.target.files);
@@ -94,14 +104,14 @@ const FormUsuario = (props) => {
               <input type="number" id="password" pattern="[0-9]*" />
               <label>E-mail</label>
               <input type="number" id="email" />
-              <div className="tag-div">
+              {/* <div className="tag-div">
                 <label>Favoritos</label>
                 <BiMinus size="1.4rem" className="less-tag" onClick={() => subTag()} />
                 <BiPlus size="1.4rem" className="plus-tag" onClick={() => addTag()} />
-              </div>
-              <input type="text" id="cat1" />
+              </div> 
+              <input type="text" id="cat1" />*/}
               {inputs}
-              <button type="submit" className="card-form-button button-ghost">
+              <button type="submit" className="card-form-button button-ghost" onClick={(e) => Cadastrar(e)}>
                 {/*TODO: Criar mensagem de usuário criado com sucesso após cadastrar  */}
                 Cadastrar
               </button>
