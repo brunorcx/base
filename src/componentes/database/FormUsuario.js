a3693d847e07b8ba6c75392e191b4f128b798ec8
Bruno RC <brunorcx@hotmail.com>
2021-11-30 21:51:58 -0400

    Corrigido erro de post e required nos inputs funcionam agora

--- src/componentes/database/FormUsuario.js
+++ src/componentes/database/FormUsuario.js
@@ -36,24 +36,26 @@ const FormUsuario = (props) => {
   }
 
   async function ImageCompression(image) {
-    const imageFile = image;
-    // const imageFile = imagemArquivo;
-    console.log("originalFile instanceof Blob", imageFile instanceof Blob); // true
-    console.log(`originalFile size ${imageFile.size / 1024 / 1024} MB`);
+    if (image != null) {
+      const imageFile = image;
+      // const imageFile = imagemArquivo;
+      console.log("originalFile instanceof Blob", imageFile instanceof Blob); // true
+      console.log(`originalFile size ${imageFile.size / 1024 / 1024} MB`);
 
-    const options = {
-      maxSizeMB: 1,
-      maxWidthOrHeight: 1024,
-      useWebWorker: true,
-    };
-    try {
-      const compressedFile = await imageCompression(imageFile, options);
-      console.log("compressedFile instanceof Blob", compressedFile instanceof Blob); // true
-      console.log(`compressedFile size ${compressedFile.size / 1024 / 1024} MB`); // smaller than maxSizeMB
+      const options = {
+        maxSizeMB: 1,
+        maxWidthOrHeight: 1024,
+        useWebWorker: true,
+      };
+      try {
+        const compressedFile = await imageCompression(imageFile, options);
+        console.log("compressedFile instanceof Blob", compressedFile instanceof Blob); // true
+        console.log(`compressedFile size ${compressedFile.size / 1024 / 1024} MB`); // smaller than maxSizeMB
 
-      return compressedFile;
-    } catch (error) {
-      console.log(error);
+        return compressedFile;
+      } catch (error) {
+        console.log(error);
+      }
     }
   }
 
@@ -63,11 +65,11 @@ const FormUsuario = (props) => {
         <h2>Cadastrar Usuário</h2>
         <div className="card-grid">
           <div className="col-50 card-cell card-login">
-            <form className="card-form" method="post" encType="multipart/form-data">
+            <form className="card-form" method="post" encType="multipart/form-data" onSubmit={(e) => Cadastrar(e)}>
               <label>Nome</label>
               <input type="text" id="name" name="name" required autoFocus />
               <label>Senha</label>
-              <input type="text" id="password" required autoFocus />
+              <input type="password" id="password" required autoFocus />
               <label>E-mail</label>
               <input type="text" id="email" required autoFocus />
               {/* <div className="tag-div">
@@ -76,7 +78,7 @@ const FormUsuario = (props) => {
                 <BiPlus size="1.4rem" className="plus-tag" onClick={() => addTag()} />
               </div> 
               <input type="text" id="cat1" />*/}
-              <button type="submit" className="card-form-button button-ghost" onClick={(e) => Cadastrar(e)}>
+              <button type="submit" className="card-form-button button-ghost">
                 {/*TODO: Criar mensagem de usuário criado com sucesso após cadastrar  */}
                 Cadastrar
               </button>
