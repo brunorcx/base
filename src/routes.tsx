006f1fa28c2c2eda476c72840e05cb7616ad46a7
Bruno RC <brunorcx@hotmail.com>
2021-12-14 21:30:50 -0400

    Atulizando Switch para routes e package.json atualizado, porém com a atualização está com vários erros

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,28 +1,25 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Produtos from "./pages/Produtos";
 import Produto from "./pages/Produto";
 import CProduto from "./pages/CadastroProduto";
 import Usuarios from "./pages/Usuarios";
 
-function Routes() {
+function RoutesFunc() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/Produtos" exact component={Produtos}></Route>
-        <Route
-          path="/Produto/:id"
-          exact
-          render={(props) => <Produto {...props} />} /*TODO https://www.youtube.com/watch?v=VD7ojK3deWE */
-        />
-        <Route path="/Usuarios" exact component={Usuarios} />
-        <Route path="/CadastroProdutos" exact component={CProduto} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Produtos" element={<Produtos />}></Route>
+        <Route path="/Produto/:id" />
+        {/* render={(props) => <Produto {...props} />} /*TODO https://www.youtube.com/watch?v=VD7ojK3deWE * */}
+        <Route path="/Usuarios" element={<Usuarios />} />
+        <Route path="/CadastroProdutos" element={<CProduto />} />
         {/* <Route path="/pagina2" component={Pagina2}> */}
-      </Switch>
+      </Routes>
     </BrowserRouter>
   );
 }
 
-export default Routes;
+export default RoutesFunc;
