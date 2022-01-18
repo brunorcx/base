a02c0244f758c7a638508a2e4f60454ed154f61c
Bruno RC <brunorcx@hotmail.com>
2022-01-17 21:14:47 -0400

    universal login and logout of Auth0 implemented with broke UI

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -1,8 +1,11 @@
 import "../styles/login.css";
 import { MdLock } from "react-icons/md";
 import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Login = () => {
+  const { loginWithRedirect } = useAuth0();
+  const { logout } = useAuth0();
   return (
     <div className="login-g">
       <form className="login-form">
@@ -11,7 +14,8 @@ const Login = () => {
         </span>
         <input type="text" placeholder="E-mail" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" />
         <input type="password" placeholder="Password" required />
-        <button>login</button>
+        <button onClick={() => loginWithRedirect()}>login</button>
+        <button onClick={() => logout({ returnTo: window.location.origin })}> </button>
         <Link to="/Produtos">Não é cadastrado? Crie uma conta.</Link>
       </form>
     </div>
