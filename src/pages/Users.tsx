ed3d297b3221a10c3720c3fc490c4c1cbed0c461
Bruno RC <brunorcx@hotmail.com>
2022-01-20 21:44:14 -0400

    Get function for userID and token to acess roles

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -3,12 +3,30 @@ import { Footer } from "../componentes/Footer";
 import React, { useState, useEffect } from "react";
 import UserTable from "../componentes/UserTable";
 import { useAuth0 } from "@auth0/auth0-react";
+var axios = require("axios").default;
 
 export interface UserProps {}
 
 const Users: React.FC<UserProps> = () => {
   const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
   const [createUser, setCreateUser] = useState(false); //Valor dentro da função é valor inicial da variável
+
+  async function GetUserAuth(userID: string, token: string) {
+    var options = {
+      method: "GET",
+      url: "https://rodriguesdev.us.auth0.com/api/v2/users/" + userID + "/roles",
+      headers: { authorization: token },
+    };
+
+    axios
+      .request(options)
+      .then(function (response: any) {
+        console.log(response.data);
+      })
+      .catch(function (error: any) {
+        console.error(error);
+      });
+  }
 
   useEffect(() => {
     if (createUser) {
@@ -24,7 +42,7 @@ const Users: React.FC<UserProps> = () => {
     <div>
       <Navbar />
       {isAuthenticated && <UserTable />}
-      {user ? console.log(user.sub) : false}
+      {console.log(user)}
       <Footer />
     </div>
   );
