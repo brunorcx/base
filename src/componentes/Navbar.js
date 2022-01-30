973ffc498695a730ae391d35ad4ee166d5bffa93
Gabriel Rodrigues Machado <grmachado12@hotmail.com>
2022-01-30 13:22:50 -0300

    Login Mobile is working (#11)
    Co-authored-by: Gabriel Machado <gramchado12@hotmail.com>

--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -21,8 +21,11 @@ const Navbar = (props) => {
     useAuth0();
 
   const [toggleCart, settoggleCart] = useState(false);
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   const [navbar2, setNavbar2] = useState("navbar2");
   const [scrollPosition, setScrollPosition] = useState(0);
+
   const handleScroll = () => {
     const position = window.pageYOffset;
     setScrollPosition(position);
@@ -51,10 +54,23 @@ const Navbar = (props) => {
     }
   }, [scrollPosition]);
 
+  useEffect(() => {
+    const nav = document.getElementById("nav");
+    if (toggleMenu == true) {
+      nav.classList.add("active");
+    } else {
+      nav.classList.remove("active");
+    }
+  }, [toggleMenu]);
+
   return (
     <header id="nav" className="nav">
       <div className="navbar1">
-        <button className="btn-mobile" id="btn-mobile">
+        <button
+          className="btn-mobile"
+          id="btn-mobile"
+          onClick={() => setToggleMenu(!toggleMenu)}
+        >
           <span id="hamburguer" className="hamburguer"></span>
         </button>
         <div className="logo"></div>
@@ -119,135 +135,3 @@ const Navbar = (props) => {
   );
 };
 export default Navbar;
-
-// export class Navbar extends Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       open: false,
-//       top: true,
-//       login: false,
-//     };
-
-//     this.refNavbar2 = React.createRef();
-//   }
-
-//   //###################### INICIO TOPO SUMIR #########################
-
-//   OffTheTop() {
-//     //Verifica a largura para não ter o efeito de sumir e aparecer no modo mobile
-//     var width =
-//       window.innerWidth ||
-//       document.documentElement.clientWidth ||
-//       document.body.clientWidth;
-
-//     if (width > 768) {
-//       //Verificar porquê window.pageYOffset dispara tantas vezes
-//       if (window.pageYOffset === 0) {
-//         this.setState({ top: true });
-//         this.refNavbar2.current.className = "navbar2";
-//       } else if (window.pageYOffset >= 100) {
-//         this.setState({ top: false });
-//         this.refNavbar2.current.className = "fade";
-//       }
-//     }
-//   }
-//   componentDidMount() {
-//     window.onscroll = this.OffTheTop.bind(this);
-//   }
-
-//   componentWillUnmount() {
-//     window.onscroll = null;
-//   }
-
-//   //######################## FIM SUMIR TOPO ##############################
-
-//   render() {
-//     /* TERMINA AQUI*/
-
-//     let login_class = this.state.login ? "login" : "closedLogin";
-
-//     let cart_class = this.state.open ? "shoppingCart" : "closedShoppingCart";
-
-//     let background_cart =
-//       this.state.open || this.state.login
-//         ? "darkBackgroundCart"
-//         : "closedDarkBackgroundCart";
-
-//     function toggleMenu() {
-//       const nav = document.getElementById("nav");
-//       nav.classList.toggle("active");
-//     }
-
-//     return (
-//       <header id="nav" className="nav">
-//         <div className="navbar1">
-//           <button className="btn-mobile" id="btn-mobile" onClick={toggleMenu}>
-//             <span id="hamburguer" className="hamburguer"></span>
-//           </button>
-//           <div className="logo"></div>
-//           <div className="rightNav">
-//             <div className="div-user" onClick={this.OpenLogin.bind(this)}>
-//               <HiOutlineUser className="user-icon" size="2rem" />
-//               <div className="welcome">
-//                 <label className="label-bv">Bem Vindo</label>
-//                 <label> Entre</label>
-//               </div>
-//               <container className="login-container">
-//                 <button className="sign-in">Entrar</button>
-//                 <button className="sign-up">Cadastrar</button>
-//               </container>
-//             </div>
-
-//             <div>
-//               <div className="imgCart">
-//                 <AiOutlineShoppingCart
-//                   className="cartIcon"
-//                   size="2rem"
-//                   onClick={this.OpenCart.bind(this)}
-//                 />
-//               </div>
-//             </div>
-//             <div className={cart_class}>
-//               <Cart />
-//             </div>
-//             <div className={login_class}>
-//               <Login />
-//             </div>
-//           </div>
-//         </div>
-
-//         <div
-//           className={background_cart}
-//           onClick={
-//             this.state.login
-//               ? this.OpenLogin.bind(this)
-//               : this.OpenCart.bind(this)
-//           }
-//         />
-
-//         <div className="horizontalLine" />
-//         <div className="navbar2" id={"navbar2"} ref={this.refNavbar2}>
-//           {/* <AiOutlineMenu className="hamburguer" size="2rem" /> */}
-//           <ul className={"menu"}>
-//             <li>
-//               <Link to="/">Home</Link>
-//             </li>
-//             <li>
-//               <Link to="/Produtos">Produtos</Link>
-//             </li>
-//             <li>
-//               <Link to="/Usuarios">Usuários</Link>
-//             </li>
-//             <li>
-//               <Link to="/CadastroProdutos">Cadastrar Produtos</Link>
-//             </li>
-//           </ul>
-//         </div>
-//       </header>
-//     );
-//   }
-// }
-
-// export default Navbar;
